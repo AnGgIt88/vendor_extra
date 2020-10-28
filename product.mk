1092a072e555533ccfbe4f781b04aa0bae001b2d
Dyneteve <dyneteve@hentaios.com>
2020-10-28 10:31:32 +0300

    extra: Introduce Flipendo ( Extreme Battery Saver )
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I88204a21e5374cae8c6fa583a3300a03427bd359

--- product.mk
+++ product.mk
@@ -16,6 +16,13 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 
+# Flipendo
+PRODUCT_COPY_FILES += \
+    vendor/extra/config/sysconfig/pixel_experience_2020.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_experience_2020.xml
+
+PRODUCT_PACKAGES += \
+    Flipendo
+
 # Google Wallpaper
 PRODUCT_PACKAGES += \
     WallpaperPickerGoogleRelease
@@ -23,3 +30,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/extra/config/permissions/googlewallpaper-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/googlewallpaper-hiddenapi-package-whitelist.xml \
     vendor/extra/config/permissions/privapp-permissions-googlewallpaper.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-googlewallpaper.xml
+
+# Overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/extra/overlay/common
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/extra/overlay/common
