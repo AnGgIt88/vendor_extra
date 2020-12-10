fad72a6b82908310788a0d6de8420ec86dbf4e86
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-12-10 20:45:09 +0530

    extra: Add Google WallpaperPicker
    Change-Id: Ib58114d7bd753e7538dce87867f8c22f77730218

--- product.mk
+++ product.mk
@@ -15,3 +15,11 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+
+# Google Wallpaper
+PRODUCT_PACKAGES += \
+    WallpaperPickerGoogleRelease
+
+PRODUCT_COPY_FILES += \
+    vendor/extra/config/permissions/googlewallpaper-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/googlewallpaper-hiddenapi-package-whitelist.xml \
+    vendor/extra/config/permissions/privapp-permissions-googlewallpaper.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-googlewallpaper.xml
