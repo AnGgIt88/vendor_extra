7cd05c78e1bf5e2ae2b6071538685fef6e2692bc
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-04-08 17:00:25 +0000

    extra: import MIUI apn-conf
    Change-Id: Id2ef3b49ade786320147a4b63bf1e16ad4e2ab73

--- product.mk
+++ product.mk
@@ -1,3 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+# apn-conf
+PRODUCT_COPY_FILES += \
+    vendor/extra/config/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
